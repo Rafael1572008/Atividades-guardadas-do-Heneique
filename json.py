3d0a50246cb08cf600eba5cb45d74401e35841c0
Rafael Pires Gonçalves <127984148+Rafael1572008@users.noreply.github.com>
2024-08-16 09:19:40 -0300

    Create json.py

---
+++ json.py
@@ -0,0 +1,12 @@
+def add_aluno():
+    aluno = {}
+    aluno[nome] = input("Nome: ")
+    
+
+def show_aluno(aluno):
+    return aluno
+
+aluno = {"Nome" : "Igor", "Idade" : 20, "Prontuario" : "sp12345", "Português" : 6, "Matemática" : 8, "Ciências" : 10, "Média" : 8}, 
+{"Nome" : "Ana", "Idade" : 19, "Prontuario" : "sp456789", "Português" : 10, "Matemática" : 3, "Ciência" : 9, "Média" : 7.4}
+
+add_aluno()
