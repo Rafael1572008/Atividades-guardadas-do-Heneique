b9d808c751021bc59c6fed23b8cfa07dcdc3a1c2
Rafael Pires Gonçalves <127984148+Rafael1572008@users.noreply.github.com>
2024-08-18 12:50:40 -0300

    Add files via upload

---
+++ 2024-SPOWEB1/hello.js
@@ -0,0 +1,22 @@
+//item
+console.log("Script Carregado")
+
+//item 2
+function calcularAnos();
+
+//item 3
+console.log("Botão de Calcular clicando")
+
+//item 4
+let popAInput = document.getElementById("popA").value.trim();
+
+//item 5
+let taxaAInput= document.getElementById("taxaA").value.trim();
+
+//item 6
+let popBInput = document.getElementById("popB").value.trim();
+
+//item 7
+let taxaBInput= document.getElementById("taxaB").value.trim();
+
+//item 8
